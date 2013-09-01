7675bedf4c077a4b506e2684c86d4b8c13b3dfd8
ChrisRimondi <chris.rimondi@gmail.com>
2013-08-31 20:44:42 -0400

    08312013

--- VAtoELSA.py
+++ VAtoELSA.py
@@ -74,8 +74,33 @@ import sys
 import getopt
 import xml.etree.ElementTree as xml
 import re
-import logging
-from logging.handlers import SysLogHandler
+import socket
+
+
+FACILITY = {
+	'kern': 0, 'user': 1, 'mail': 2, 'daemon': 3,
+	'auth': 4, 'syslog': 5, 'lpr': 6, 'news': 7,
+	'uucp': 8, 'cron': 9, 'authpriv': 10, 'ftp': 11,
+	'local0': 16, 'local1': 17, 'local2': 18, 'local3': 19,
+	'local4': 20, 'local5': 21, 'local6': 22, 'local7': 23,
+}
+
+LEVEL = {
+	'emerg': 0, 'alert':1, 'crit': 2, 'err': 3,
+	'warning': 4, 'notice': 5, 'info': 6, 'debug': 7
+}
+
+def syslog(message, level=LEVEL['notice'], facility=FACILITY['daemon'],
+	host='localhost', port=514):
+
+	"""
+	Send syslog UDP packet to given host and port.
+	"""
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	data = '<%d>%s' % (level + facility*8, message)
+	sock.sendto(data.encode('utf-8'), (host, port))
+	sock.close()
 
 class OpenVasParser:
 	"This clas will parse an OpenVAS XML file and create an object"
@@ -101,18 +126,18 @@ class OpenVasParser:
 			issueDict = {};
 			if result.find('host') is not None:
 				issueDict['host'] = unicode(result.find('host').text)
-				issueDict['full_text'] = unicode(result.find('host').text)
+				issueDict['full_text'] = 'Host: ' + unicode(result.find('host').text)
 				#print issueDict['host']
 			for nvt in result.iter('nvt'):
 				issueDict['oid'] = unicode(nvt.attrib['oid'])
-				issueDict['full_text'] += ' | ' + unicode(nvt.attrib['oid'])
+				issueDict['full_text'] += ' | ' + 'OID: ' + unicode(nvt.attrib['oid'])
 				for child in nvt:
 					issueDict[child.tag] = unicode(child.text)
-					issueDict['full_text'] += ' | ' + unicode(child.text)
+					issueDict['full_text'] += ' | ' + unicode(child.tag) + ': ' + unicode(child.text)
 			
 			if result.find('description') is not None:
 				issueDict['description'] = unicode(result.find('description').text)
-				issueDict['full_text'] += ' | ' + unicode(result.find('description').text)
+				issueDict['full_text'] += ' | ' + 'Description: ' + unicode(result.find('description').text)
 			if issueDict:
 				issuesList.append(issueDict)
 		
@@ -166,6 +191,23 @@ class OpenVasParser:
 				stringCSV += unicode(value) + ','
 			stringCSVList.append(unicode(stringCSV))
 		return stringCSVList
+
+class OpenVaslogger:
+	"This clas will take in an object from OpenVASParser and log it to syslog"
+	
+	def __init__(self, np):
+		self.np_parsed = np
+		#init syslog capabilities
+		
+	def toSyslog(self):
+		for item in self.np_parsed.issueList:
+			if item['risk_factor'] == 'None':
+				syslog('openvas: ' + 'Attribute| ' + item['full_text'], host='192.168.1.116')
+			else:
+				syslog('openvas: ' + 'Vulnerability| ' + item['full_text'], host='192.168.1.116')
+		for port in self.np_parsed.portList:
+			#print port['full_text']
+			syslog('openvas: ' + 'Attribute|' + port['full_text'], host='192.168.1.116')
 
 class NessusParser:
 	"This clas will parse an Nessus v2 XML file and create an object"
@@ -300,21 +342,12 @@ class NessusParser:
 class NessusSyslogger:
 	"This clas will take in an object from NessusParser and log it to syslog"
 	
-	def __init__(self, np, elsa_class_num=10003):
+	def __init__(self, np):
 		self.np_parsed = np
-		self.elsa_class_num = elsa_class_num
-		#init syslog capabilities
-		self.logger = logging.getLogger()
-		self.logger.setLevel(logging.INFO)
-		self.syslog = SysLogHandler(address=('192.168.1.116', 514))
-		self.formatter = logging.Formatter('VA: %(message)s')
-		self.syslog.setFormatter(self.formatter)
-		self.logger.addHandler(self.syslog)
 		
 	def toSyslog(self):
 		for item in self.np_parsed.issueList:
-			#print item['full_text']
-			self.logger.info(item['full_text'])
+			syslog('nessus: ' + item['full_text'], host='192.168.1.116')
 		
 	
 		
@@ -325,59 +358,121 @@ class NessusSyslogger:
 def create_sql_file():
 	sql_file = """
 use syslog;
-INSERT INTO classes (id, class) VALUES (10201, "VULNERABILITY");
-INSERT INTO classes (id, class) VALUES (10202, "ASSET_ATTRIBUTE");
-INSERT INTO fields (field, field_type, pattern_type) VALUES ("operating_system", "string", "QSTRING");
+INSERT INTO classes (id, class) VALUES (10201, "NESSUS");
+INSERT INTO fields (field, field_type, pattern_type) VALUES ("exp_avail", "int", "NUMBER");
+INSERT INTO fields (field, field_type, pattern_type) VALUES ("cvss_base", "int", "NUMBER");
+INSERT INTO fields (field, field_type, pattern_type) VALUES ("cvss_temporal", "int", "NUMBER");
+INSERT INTO fields (field, field_type, pattern_type) VALUES ("nid", "string", "QSTRING");
+INSERT INTO fields (field, field_type, pattern_type) VALUES ("risk_factor", "string", "QSTRING");
 INSERT INTO fields (field, field_type, pattern_type) VALUES ("cve", "string", "QSTRING");
 
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="ASSET_ATTRIBUTE"), (SELECT id FROM fields WHERE field="srcip"), 5);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="ASSET_ATTRIBUTE"), (SELECT id FROM fields WHERE field="srcport"), 6);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="ASSET_ATTRIBUTE"), (SELECT id FROM fields WHERE field="proto"), 7);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="ASSET_ATTRIBUTE"), (SELECT id FROM fields WHERE field="macb"), 11);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="ASSET_ATTRIBUTE"), (SELECT id FROM fields WHERE field="hostname"), 12);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="ASSET_ATTRIBUTE"), (SELECT id FROM fields WHERE field="operating_system"), 13);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="ASSET_ATTRIBUTE"), (SELECT id FROM fields WHERE field="service"), 14);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="ASSET_ATTRIBUTE"), (SELECT id FROM fields WHERE field="desc"), 15);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="ASSET_ATTRIBUTE"), (SELECT id FROM fields WHERE field="notes"), 15);
 
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="exp_avail"), 5);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="srcport"), 6);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="cvss_base"), 7);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="srcip"), 8);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="severity"), 9);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="proto"), 10);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="rule"), 11);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="nid"), 12);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="desc"), 13);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="service"), 14);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="risk_factor"), 15);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="NESSUS"), (SELECT id FROM fields WHERE field="cve"), 16);
 
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="VULNERABILITY"), (SELECT id FROM fields WHERE field="srcip"), 5);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="VULNERABILITY"), (SELECT id FROM fields WHERE field="srcport"), 6);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="VULNERABILITY"), (SELECT id FROM fields WHERE field="severity"), 7);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="VULNERABILITY"), (SELECT id FROM fields WHERE field="proto"), 8);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="VULNERABILITY"), (SELECT id FROM fields WHERE field="rule"), 11);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="VULNERABILITY"), (SELECT id FROM fields WHERE field="desc"), 12);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="VULNERABILITY"), (SELECT id FROM fields WHERE field="service"), 13);
-INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="VULNERABILITY"), (SELECT id FROM fields WHERE field="cve"), 14);
+INSERT INTO classes (id, class) VALUES (10202, "OPENVAS");
+INSERT INTO fields (field, field_type, pattern_type) VALUES ("oid", "string", "QSTRING");
 
-"""
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="OPENVAS"), (SELECT id FROM fields WHERE field="srcip"), 5);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="OPENVAS"), (SELECT id FROM fields WHERE field="srcport"), 6);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="OPENVAS"), (SELECT id FROM fields WHERE field="cvss_base"), 7);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="OPENVAS"), (SELECT id FROM fields WHERE field="proto"), 11);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="OPENVAS"), (SELECT id FROM fields WHERE field="oid"), 12);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="OPENVAS"), (SELECT id FROM fields WHERE field="vuln_desc"), 13);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="OPENVAS"), (SELECT id FROM fields WHERE field="service"), 14);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="OPENVAS"), (SELECT id FROM fields WHERE field="risk_factor"), 15);
+INSERT INTO fields_classes_map (class_id, field_id, field_order) VALUES ((SELECT id FROM classes WHERE class="OPENVAS"), (SELECT id FROM fields WHERE field="cve"), 16);
+""" 
+
 	f = open('va_db_setup.sql','w')
 	f.write(sql_file)
 	f.close()
-	
+
+def create_xml_file():
+	xml_file = """
+<ruleset>
+	<pattern>nessus</pattern>
+    	<rules>
+        	<rule class="10201" id="10201">
+                  <patterns>
+                       <pattern>@ESTRING::: @@ESTRING:i3: | @@ESTRING::: @@ESTRING:i1: | @@ESTRING::: @@ESTRING:s3: | @@ESTRING::: @@ESTRING:i5: | @@ESTRING::: @@ESTRING:i4: | @@ESTRING:::
+: @@ESTRING:s1: | @@ESTRING::: @@ESTRING:s0: | @@ESTRING::: @@ESTRING:: | @@ESTRING::: @@ESTRING:i0: | @@ESTRING::: @@ESTRING:: | @@ESTRING::: @@ESTRING:: | @@ESTRING::: @@ESTRING:s4: | @@EE
+STRING::: @@ANYSTRING:s2:@</pattern>
+                 </patterns>
+                 <examples>
+                	<example>
+                    	<test_message program="nessus">IP: 192.168.1.115 | Port: 445 | SVC: cifs | Protocol: tcp | Severity: 0 | NID: 10394 | Plugin Name: Microsoff
+t Windows SMB Log In Possible | Plugin Family: Windows | Public Exploit Available: true | Plugin Modification Date: 2013/04/23 | Plugin Type: remote | Risk Factor: None | See also: http://
+/support.microsoft.com/kb/143474</test_message>
+                                                <!-- eventid -->
+                                                <test_value name="s0">QMOWsHjZqde</test_value>
+                                                <!-- srcip -->
+                                                <test_value name="i0">192.168.1.1</test_value>
+                                                <!-- srcport -->
+                                                <test_value name="i1">514</test_value>
+                                                <!-- dstip -->
+                                                <test_value name="i2">192.168.1.116</test_value>
+                                                <!-- dstport -->
+                                                <test_value name="i3">514</test_value>
+                                                <!-- proto -->
+                                                <test_value name="i4">UDP</test_value>
+                                                <!-- bro_facility -->
+                                                <test_value name="s1">LOCAL0</test_value>
+                                                <!-- bro_severity -->
+                                                <test_value name="s2">INFO</test_value>
+                                                <!-- bro_message -->
+                                                <test_value name="s3">Aug  3 23:13:39 pf: 00:00:00.804184 rule 36/0(match): pass in on vr0: (tos 0x0, ttl 64, id 11232, offset 0, flags [DFF
+], proto UDP (17), length 55)   192.168.1.116.43172 > 192.168.1.1.53: 40972+ A? localhost. (27)</test_value>
+                                        </example>
+                                </examples>
+                        </rule>
+                </rules>
+        </ruleset>
+""" 
+
+	f = open('va_db_setup.xml','w')
+	f.write(xml_file)
+	f.close()	
 def usage():
-		print "Usage: VAtoELSA.py [-i input_file | --input_file=input_file] [-s | --create-sql-file] [-h | --help]"
+		print "Usage: VAtoELSA.py [-i input_file | --input_file=input_file]  [-r report_type | --report_type=type] [-s | --create-sql-file] [-h | --help]"
 def main():
 
-	letters = 'i:sh' #input_file, output_file, elsa_class_num respectively
-	keywords = ['input-file=', 'create-sql-file', 'help' ]
+	letters = 'i:r:sh' #input_file, report_type, create_sql
+	keywords = ['input-file=', 'report_type=', 'create-sql-file', 'help' ]
 	try:
 		opts, extraparams = getopt.getopt(sys.argv[1:], letters, keywords)
 	except getopt.GetoptError, err:
 		print str(err)
 		usage()
 		sys.exit()
-	in_file = 'report.nessus'
+	in_file = ''
+	report_type = 'nessus'
 	make_sql_file = False
 	
 	for o,p in opts:
-	  if o in ['-i','--input-file']:
+	  if o in ['-i','--input-file=']:
 		 in_file = p
+	  elif o in ['r', '--report_type=']:
+	  	report_type = p
 	  elif o in ['-h', '--help']:
 		 usage()
 		 sys.exit()
 	  elif o in ['-s', '--create-sql-file']:
-			make_sql_file = True
+		make_sql_file = True
+	  
+	
+	print report_type
+	print in_file
 	
 	if (len(sys.argv) < 2):
 		usage()
@@ -389,10 +484,28 @@ def main():
 		print "Input file does not exist. Exiting."
 		sys.exit()
 	
-	np = NessusParser(in_file)
-	syslogger = NessusSyslogger(np)
 	if make_sql_file == True:
 		create_sql_file()
+		print 'SQL file created as va_db_setup.sql \n'
+		print 'Usage: # mysql < va_db_setup.sql'
+		sys.exit()
+	
+	if report_type.lower() == 'nessus':
+		np = NessusParser(in_file)
+		syslogger = NessusSyslogger(np)
+	elif report_type.lower() == 'openvas':
+		np = OpenVasParser(in_file)
+		syslogger = OpenVaslogger(np)
+	elif report_type.lower() == 'nikto':
+		np = NiktoParser(in_file)
+		syslogger = Niktologger(np)
+	elif report_type.lower() == 'nmap':
+		np = NmapParser(in_file)
+		syslogger == Nmaplogger(np)
+	else:
+		print "Error: Invalid report type specified. Available options: nessus, openvas, nikto, nmap"
+		sys.exit()
+	
 	syslogger.toSyslog()
 
 if __name__ == "__main__":
